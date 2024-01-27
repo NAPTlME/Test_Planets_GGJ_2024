c8cb0bb8c2f30e6e953ee0990ef60dd8fc83cdfc
Joohyun Lee <1joohyun.lee@gmail.com>
2024-01-26 22:07:33 -0800

    wip

--- Assets/Scripts/Objects_On_Planet.cs
+++ Assets/Scripts/Objects_On_Planet.cs
@@ -2,53 +2,69 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
-
+using System.Net;
 
 [RequireComponent(typeof(Rigidbody))]
 public class Objects_On_Planet : MonoBehaviour
 {
-    public Transform HomePlanet; // change to Planet
-    public Transform OtherPlanet; // to remove and use the list instead
-    public List<Planet> OtherPlanets; // potentially convert to a dictionary instead in which the key is the Planet and the value is the distance to that planet (surface) in the last update (for calculating the impulse)
-    public float OtherPlanet_rad = 2f;
-    public float planet_rad = 1.75f;
-    public float MinDistToFeelGravity = 7f;
+    public Planet homePlanet; // change to Planet
+    public List<Planet> planets; // potentially convert to a dictionary instead in which the key is the Planet and the value is the distance to that planet (surface) in the last update (for calculating the impulse)
 
-    public float distToOtherPlanet = 0f;
-
-    public float distAccelCoef = 1f;
-    public float centerOfMassGizmoRad = 0.04f;
+    public const float DIST_ACCEL_COEF = 1f;
+    public const float CENTER_OF_MASS_GIZMO_RAD = 0.04f;
+    public const float BASE_GRAVITY_COEF = 9f;
 
     Rigidbody rbody;
 
     // Start is called before the first frame update
     void Start()
     {
+        planets = new List<Planet>();
         rbody = GetComponent<Rigidbody>();
-        transform.SetParent(HomePlanet);
+        transform.SetParent(homePlanet.transform);
     }
 
-    // Update is called once per frame
+    // FixedUpdate runs on a predetermined tick rate (50hz by default)
     void FixedUpdate()
     {
-        if (Vector3.Distance(HomePlanet.position, transform.position) - planet_rad > Vector3.Distance(OtherPlanet.position, transform.position) - OtherPlanet_rad)
+        var newPlanetDistances = planets.Select(planet => (planet, Vector3.Distance(planet.transform.position, transform.position) - planet.radius)).ToList();
+        
+        // set minimum distance planet as home planet
+        var minPlanetDistance = newPlanetDistances[0];
+        newPlanetDistances.ForEach(planetDistance =>
         {
-            var newOtherPlanet = HomePlanet;
-            HomePlanet = OtherPlanet;
-            OtherPlanet = newOtherPlanet;
-            distToOtherPlanet = 0f;
-            transform.SetParent(HomePlanet);
-        }
-        var directionToPlanet = (HomePlanet.position - transform.position).normalized;
-        var gravityForceFromPlanet = directionToPlanet * 9f;
+            if (planetDistance.Item2 < minPlanetDistance.Item2)
+            {
+                minPlanetDistance = planetDistance;
+            }
+        });
+        homePlanet = minPlanetDistance.Item1;
+        transform.SetParent(homePlanet.transform);
+
+        // force from home planet
+        var directionToPlanet = (homePlanet.transform.position - transform.position).normalized;
+        var gravityForceFromPlanet = directionToPlanet * BASE_GRAVITY_COEF;
         var totalGravityForce = gravityForceFromPlanet;
+
+        // sum up forces from non home planets
+        planets.ForEach(planet =>
+        {
+            if (GameObject.ReferenceEquals(planet, homePlanet)) {
+                return;
+            }
+            var directionToPlanet = (homePlanet.transform.position - transform.position).normalized;
+            var gravityForceFromPlanet = directionToPlanet * BASE_GRAVITY_COEF;
+            var totalGravityForce = gravityForceFromPlanet;
+            var ToOtherPlanet = (planet.transform.position - transform.position);
+        });
+        
 
         var ToOtherPlanet = (OtherPlanet.position - transform.position);
 
         var planetDistances = OtherPlanets.Select(planet => (planet, Vector3.Distance(planet.transform.position, transform.position) - planet.radius)).ToList();
         // define home planet based on closest?
         //Planet = planetDistances.OrderBy(sel => sel.Item2).Select(sel => sel.planet);
-        planetDistances.ForEach(s =>
+        newPlanetDistances.ForEach(s =>
         {
             // gravity calc here,
             // add to totalGravityForce
@@ -57,7 +73,7 @@ public class Objects_On_Planet : MonoBehaviour
         {
             var directionToOtherPlanet = ToOtherPlanet.normalized;
             var newDist = ToOtherPlanet.magnitude;
-            var accelCoef = Mathf.Max(distToOtherPlanet - newDist, 0) * distAccelCoef;
+            var accelCoef = Mathf.Max(distToOtherPlanet - newDist, 0) * DIST_ACCEL_COEF;
             var gravityForceMag = (accelCoef + 1) / Mathf.Pow(newDist, 2f) * Time.fixedDeltaTime;
             
             if (accelCoef > 0.01f) //extra impulse to get interesting behavior 
@@ -65,7 +81,7 @@ public class Objects_On_Planet : MonoBehaviour
                 rbody.AddForce(directionToOtherPlanet * accelCoef, ForceMode.Impulse);
             }
 
-            var gravityForceFromOtherPlanet = directionToOtherPlanet * 9f * gravityForceMag;
+            var gravityForceFromOtherPlanet = directionToOtherPlanet * BASE_GRAVITY_COEF * gravityForceMag;
 
             totalGravityForce += gravityForceFromOtherPlanet;
             distToOtherPlanet = newDist;
@@ -73,21 +89,19 @@ public class Objects_On_Planet : MonoBehaviour
         rbody.AddForce(totalGravityForce, ForceMode.Acceleration);
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider obj)
     {
-        if (other.tag == "planet")
+        if (obj.tag == "Planet")
         {
-            Planet otherPlanet = other.GetComponent<Planet>();
-            OtherPlanets.Add(otherPlanet);
+            planets.Add(obj.GetComponent<Planet>());
         }
     }
 
-    private void OnTriggerExit(Collider other)
+    private void OnTriggerExit(Collider obj)
     {
-        if (other.tag == "planet")
+        if (obj.tag == "Planet")
         {
-            Planet otherPlanet = other.GetComponent<Planet>();
-            OtherPlanets.Remove(otherPlanet);
+            planets.Remove(obj.GetComponent<Planet>());
         }
     }
     private void OnDrawGizmos()
@@ -96,7 +110,7 @@ public class Objects_On_Planet : MonoBehaviour
         {
             var CoM = rbody.worldCenterOfMass;
 
-            Gizmos.DrawSphere(CoM, centerOfMassGizmoRad);
+            Gizmos.DrawSphere(CoM, CENTER_OF_MASS_GIZMO_RAD);
         }
     }
 }
