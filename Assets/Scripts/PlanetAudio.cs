9da3db3848b3def0ed8c5ca031e46f74f9126500
TD <plop@plop.fake>
2024-01-27 19:19:14 -0800

    Reverting emptied PlanetAudio behavior

--- Assets/Scripts/PlanetAudio.cs
+++ Assets/Scripts/PlanetAudio.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlanetAudio : MonoBehaviour
 {
+    private AudioSource audioSource;
 
     void Start()
     {
+        audioSource = GetComponent<AudioSource>();
     }
     private void OnEnable()
     {
@@ -15,4 +18,13 @@ public class PlanetAudio : MonoBehaviour
     {
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject == GravityManager.getInstance().gameObject)
+        {
+            Debug.Log("Crashing into the sun, playing SFX");
+            audioSource.Play();
+            Destroy(this.gameObject, audioSource.clip.length);
+        }
+    }
 }
