2614eb63efb6b29460973017de1874a2bc47dbbb
Joohyun Lee <1joohyun.lee@gmail.com>
2024-01-28 11:59:34 -0800

    start in launch mode

--- Assets/Scripts/Camera/CameraManager.cs
+++ Assets/Scripts/Camera/CameraManager.cs
@@ -22,16 +22,16 @@ public class CameraManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        currentMainCamera = mainPlanetCamera;
+        currentMainCamera = mainLaunchCamera;
         currentMiniCamera = miniLaunchCamera;
         currentMainCamera.Priority = 1;
         currentMiniCamera.Priority = 1;
 
-        launchRig.isFocused = false;
-        planetCameraRig.isFocused = true;
+        launchRig.isFocused = true;
+        planetCameraRig.isFocused = false;
 
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
     }
 
     // Update is called once per frame
