e7b6a05e27104309566e6157cb1befa0d16c46d7
TD <plop@plop.fake>
2024-01-27 19:16:23 -0800

    Killed Residents and Score

--- Assets/Scripts/PlanetAudio.cs
+++ Assets/Scripts/PlanetAudio.cs
@@ -2,14 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
 public class PlanetAudio : MonoBehaviour
 {
-    private AudioSource audioSource;
 
     void Start()
     {
-        audioSource = GetComponent<AudioSource>();
     }
     private void OnEnable()
     {
@@ -18,13 +15,4 @@ public class PlanetAudio : MonoBehaviour
     {
     }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag == "Sun")
-        {
-            Debug.Log("Crashing into the sun, playing SFX");
-            audioSource.Play();
-            Destroy(this.gameObject, audioSource.clip.length);
-        }
-    }
 }
