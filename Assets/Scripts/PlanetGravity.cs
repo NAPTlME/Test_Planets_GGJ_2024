c6b3c5f65f0d509ad74d514bf0ee21a33bc9684d
TD <plop@plop.fake>
2024-01-28 17:43:17 -0800

    Fixing lost planets and pullback force

--- Assets/Scripts/PlanetGravity.cs
+++ Assets/Scripts/PlanetGravity.cs
@@ -28,24 +28,28 @@ public class PlanetGravity : MonoBehaviour
             var vector = this.gameObject.transform.position - sunPosition;
             var distanceFromSun = vector.magnitude;
             distanceToSun = distanceFromSun;
-            if (distanceFromSun > GravityManager.getInstance().MaxDistanceBeforeLost)
+            if (distanceFromSun > GravityManager.MaxDistanceBeforeLost)
             {
                 destroyed = true;
                 StatsManager.getInstance().LostPlanet(
                     this.gameObject.GetComponent<Planet>().planetType);
                 Destroy(this.gameObject, 3); // Disappear in 3 secs
             }
-            else if (distanceFromSun > GravityManager.getInstance().MaxDistanceBeforeBending)
+            else
+
+            if (distanceFromSun > GravityManager.MaxDistanceBeforeBending)
             {
                 //Debug.Log(Time.frameCount + "  " + distanceFromSun);
                 //Debug.Log("Adding perpendicular force!");
                 // var vectorPerpendicularToTrajectory = Vector3.Cross(vector, Vector3.back);
                 var vectorPerpendicularToTrajectory = new Vector3(vector.z, 0, vector.x * -1);
-                rigidBody.AddForce(
-                    GravityManager.getInstance().BendingForce * (vector / distanceFromSun * -1
-                    / GravityManager.getInstance().PullBackToPerpendicularRatio +
-                    vectorPerpendicularToTrajectory) * rigidBody.mass
-                );
+                var force = GravityManager.BendingForce
+                * (vector / distanceFromSun * -1
+                    / GravityManager.PullBackToPerpendicularRatio +
+                    vectorPerpendicularToTrajectory)
+                    * rigidBody.mass;
+                Debug.Log(force);
+                rigidBody.AddForce(force);
             }
         }
     }
